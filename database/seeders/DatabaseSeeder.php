01e111b6825a6c600e44cffb1d9e41c7d0e38645
Tyastiana <putrytiana125@gmail.com>
2024-12-26 23:46:13 +0700

    init final

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // Create Roles
+        $roles = ['admin', 'guest'];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        // Create user and assign admin role
+        $adminUser = User::create([
+            'name' => 'admin',
+            'email' => 'admin@chirper.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => Carbon::now()->toDateTimeString()
+        ]);
+
+        $adminUser->assignRole('admin');
+
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'user@chirper.com',
+        ]);
+
+        $user->assignRole('guest');
+
+    }
+}
---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'role:admin'])->name('admin')->group(function () {
+    Route::get('admin/users/index', [DashboardController::class, 'index'])->name('users.index');
+    Route::get('admin/users/create', [DashboardController::class, 'create'])->name('users.create');
+});
